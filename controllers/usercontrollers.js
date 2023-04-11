45c39ee1f339c2119e4b024c28176b4978d05b25
dltorrise <dltorrise@gmail.com>
2023-04-11 17:11:47 -0500

    removeFriend route completed

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -25,8 +25,31 @@ const usercontrollers = {
 
     async addFriend (req, res) {
         try {
+            //want to change it so you find them by _id and username in friendslist
             const newFriend = await Users.findOneAndUpdate({userId: req.params.id}, {$addToSet: {friends: req.body.userId}})
             res.json("Congratulations! You made a new freind!")
+        }catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    },
+
+    async removeFriend (req, res) {
+        try {
+            //want to change it so you find them by _id and username in friendslist
+            const deletedFriend = await Users.findOneAndUpdate({userId: req.params.id}, {$pull: {friends: req.body.userId}})
+            //how to reference specific person
+            res.json("You have removed this person as a friend")
+        }catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    },
+
+    async deleteUser (req, res) {
+        try {
+            const exUser = await Users.findOneAndDelete({userId: req.params.id})
+            res.json("We are sorry to see you go. Feel free to sign up again any time!")
         }catch (err) {
             console.log(err)
             res.status(500).json(err)
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,17 +3,29 @@ const router = require('express').Router();
 const {
    getUsers,
    addUser,
-   addFriend
+   addFriend,
+   removeFriend,
+   deleteUser
 } = require('../controllers/usercontrollers') //add path
 
 // /api/users
 
-//route to get users and add users
+//route to get all users and add new users
 router.route('/').get(getUsers).post(addUser)
 
 //route to add friends
-
 router.route('/add-friend/:id').post(addFriend)
+
+//route to remove friends
+//it is a put route not a delete route because even though
+//you are removing a friend, you are really updating a user
+router.route('/remove-friend/:id').put(removeFriend)
+
+//route to get a specific user, update them, or delete them
+//pull them from friend's lists
+router.route('/:id').delete(deleteUser)
+
+
 
 
 module.exports = router;
